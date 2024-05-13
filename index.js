165dc9c3226784d63b0ed3b5391f0cd0c3b4ea46
developer20010605 <119443988+developer20010605@users.noreply.github.com>
2024-05-14 06:14:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import { admin } from './firebaseAdmin';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { admin } = require('./firebaseAdmin');
 
 const app = express();
-
-// Allow all origins
 app.use(cors());
+app.use(bodyParser.json());
 
-// Your API route for phone authentication
 app.post('/api/phoneAuth', async (req, res) => {
-  if (req.method === 'POST') {
-    const { phoneNumber } = req.body;
+  const { phoneNumber } = req.body;
 
-    try {
-      const number = "+976" + phoneNumber; // Adjust country code accordingly
-      const confirmationResult = await admin.auth().signInWithPhoneNumber(number);
-
-      // Here you might want to save `confirmationResult` or send it back to the client for further processing
-
-      res.status(200).json({ success: true, message: 'Phone authentication initiated.' });
-    } catch (error) {
-      console.error('Error in phoneAuth:', error);
-      res.status(500).json({ success: false, message: error.message });
-    }
-  } else {
-    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  try {
+    const number = "+976" + phoneNumber; // Adjust country code accordingly
+    const confirmationResult = await admin.auth().signInWithPhoneNumber(number);
+    res.status(200).json({ success: true, message: 'Phone authentication initiated.' });
+  } catch (error) {
+    console.error('Error in phoneAuth:', error);
+    res.status(500).json({ success: false, message: error.message });
   }
 });
 
