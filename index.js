c28dbeec556dab1cb57c7acf5a6a302c5284bf03
developer20010605 <119443988+developer20010605@users.noreply.github.com>
2024-05-14 07:10:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ import admin from './firebaseAdmin'; // Import Firebase Admin SDK
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    // Preflight request, respond with 200 OK
-    res.status(200).end();
-    return;
-  }
-
   if (req.method === 'POST') {
     const { phoneNumber } = req.body;
 
