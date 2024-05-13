1e52879a047f21d7e9deeb5c977ec0a7e12b1506
developer20010605 <119443988+developer20010605@users.noreply.github.com>
2024-05-14 05:18:21 +0800

    Create firebaseAdmin.js

---
+++ firebaseAdmin.js
@@ -0,0 +1,25 @@
+// lib/firebaseAdmin.js
+
+import admin from 'firebase-admin';
+
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "your-project-id",
+  "private_key_id": "your-private-key-id",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nYourPrivateKeyHere\n-----END PRIVATE KEY-----\n",
+  "client_email": "your-client-email@your-project-id.iam.gserviceaccount.com",
+  "client_id": "your-client-id",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/your-client-email%40your-project-id.iam.gserviceaccount.com"
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://your-project-id.firebaseio.com" // Replace with your database URL
+  });
+}
+
+export { admin };
